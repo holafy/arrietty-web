3c1b999fc61fa58e27378117f689553f9fcf9a3f
ivy <holaivy@gmail.com>
2013-10-15 11:13:16 +0800

    Signed-off-by: ivy <holaivy@gmail.com>

--- src/main/java/ivy/web/msg/holder/IMessageLoader.java
+++ src/main/java/ivy/web/msg/holder/IMessageLoader.java
@@ -2,8 +2,17 @@ package ivy.web.msg.holder;
 
 import java.util.Map;
 
+/**
+ * Message加载器接口
+ * 
+ * @author holaivy@gmail.com
+ * 
+ */
 public interface IMessageLoader {
 
 	public abstract void fill(Map<String, String> map);
-	
+
+	public abstract int getMode();
+
+	public abstract void setMode(int mode);
 }
